1780ec2158ec3936fd4a0059b5c9bda7e6fbe91f
Harold Mosquera <haroldmosquera739@gmail.com>
2023-11-17 17:35:17 -0500

    configure api

--- src/pages/installation/InstallationPage.tsx
+++ src/pages/installation/InstallationPage.tsx
@@ -1,10 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 type Props = {};
 
 const InstallationPage = (props: Props) => {
+  const [autos, setAutos] = useState([]);
+
+  useEffect(() => {
+    axios.get('https://backen-diplomado-51d51f42ca0d.herokuapp.com/autos/')
+      .then((response) => {
+        console.log(response.data);
+        setAutos(response.data);
+      })
+      .catch((error) => {
+        console.error('There was an error!', error);
+      });
+  }, []);
+
   return (
-    <div>InstallationPage</div>
+    <div>
+      {autos.map((auto, index) => (
+        <div key={index}>
+          {}
+        </div>
+      ))}
+    </div>
   );
 };
 
