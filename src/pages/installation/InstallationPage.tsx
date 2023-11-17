456db63371426284c22248d3684577bd9dc86b4f
Harold Mosquera <haroldmosquera739@gmail.com>
2023-11-17 18:00:29 -0500

    api connect

--- src/pages/installation/InstallationPage.tsx
+++ src/pages/installation/InstallationPage.tsx
@@ -2,30 +2,35 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
 type Props = {};
+  interface Auto {
+    placa: string;
+    marca: string;
+    cantidad_pasajeros:string
+    // Add other properties here if needed
+  }
 
-const InstallationPage = (props: Props) => {
-  const [autos, setAutos] = useState([]);
+  const InstallationPage = (props: Props) => {
+    const [autos, setAutos] = useState<Auto[]>([]);
 
-  useEffect(() => {
-    axios.get('https://backen-diplomado-51d51f42ca0d.herokuapp.com/autos/')
-      .then((response) => {
-        console.log(response.data);
-        setAutos(response.data);
-      })
-      .catch((error) => {
-        console.error('There was an error!', error);
-      });
-  }, []);
+    useEffect(() => {
+      axios.get('https://backen-diplomado-51d51f42ca0d.herokuapp.com/autos/')
+        .then((response) => {
+          console.log(response.data);
+          setAutos(response.data);
+        })
+        .catch((error) => {
+          console.error('There was an error!', error);
+        });
+    }, []);
 
-  return (
-    <div>
-      {autos.map((auto, index) => (
-        <div key={index}>
-          {}
-        </div>
-      ))}
-    </div>
-  );
-};
-
+    return (
+      <div>
+        {autos.map((auto, index) => (
+          <div key={index}>
+            {auto.placa} - {auto.marca} - {auto.cantidad_pasajeros}
+          </div>
+        ))}
+      </div>
+    );
+  };
 export default InstallationPage;
